4ac62f33f3fe233554f39758d12eddf0ab41e272
paulr <paulr@DESKTOP-1P6Q0OA>
2024-09-13 02:24:07 +0800

    Add project files.

---
+++ ContactSphere.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ContactSphere.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
